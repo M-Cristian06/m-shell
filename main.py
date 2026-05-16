bb7c3190c475d2e37ce020274760efa85e28f324
M-Cristian06 <mironcristianionel@gmail.com>
2026-05-17 02:48:55 +0300

    Resolve rq>>url command & Add speed>>test command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests as r 
+import speedtest
 
 VERSION = "0.0.1"
 print(f"-- m-shell --")
@@ -9,10 +10,25 @@ commands = (
 )
 
 
+def speed_command():
+    speed_test = speedtest.Speedtest()
+
+    print("Finding the best server")
+    speed_test.get_best_server()
+
+    download_speed = speed_test.download() / 1000000
+
+    upload_speed = speed_test.upload() / 1000000
+    
+    print(f"Download speed: {download_speed:.2f} Mbps | Upload speed: {upload_speed:.2f} Mbps")
+
+
 def rq_command(url: str):
     request = r.get(url=url)
-    print(request.status_code)
-
+    if request.status_code == 200:
+        print(f"{request.status_code} | OK!")
+    else:
+        print(f"{request.status_code}")
 
 def command_processor():
     command = input("COMMAND -> ")
@@ -21,8 +37,13 @@ def command_processor():
     elif command == "info>>help":
         print("commands: ")
         print(commands)
-    elif command == f"rq>>":
-         pass 
+    elif command == "rq>>url":
+         url = input("URL(e.g. 'https://url.com'):")
+         rq_command(url)
+    elif command == "speed>>test":
+        speed_command()
+
+
 
 
 
